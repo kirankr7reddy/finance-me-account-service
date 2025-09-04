d23b0872b0af56b6a20b2fafb1e1e94f11da9865
kirankr7reddy <kondreddykr7@gmail.com>
2025-09-04 15:02:36 +0530

    Update AccountService.java

--- src/main/java/com/financeme/accountservice/service/AccountService.java
+++ src/main/java/com/financeme/accountservice/service/AccountService.java
@@ -4,7 +4,7 @@ import com.financeme.accountservice.model.Account;
 import com.financeme.accountservice.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 import java.util.Optional;
 
 @Service
